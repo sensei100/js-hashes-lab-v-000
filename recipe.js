961fd15ccc19c43ea4b40b5c780fe4dbf60c327d
sensei100 <jenanton01@gmail.com>
2016-09-15 21:00:25 -0400

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,27 @@
 'use strict';
+
+var recipe = {ingredient: "amount"}
+
+function addIngredient(recipe, ingredient, amount) {
+  var recipe = {chocolate: '16 oz'}
+  return recipe;
+}
+
+function removeIngredient(recipe, ingredient) {
+  var recipe = {butter: '2 sticks', sugar: '2 cups'}
+ delete recipe[ingredient];
+ return recipe;
+}
+
+function updateIngredient(recipe, ingredient, amount) {
+  var recipe = { butter: '2 sticks', vanilla: 'one teaspoon', sugar: '3 cups' }
+  recipe[ingredient] = amount;
+  return recipe;
+}
+
+function readRecipe(recipe) {
+  var recipe = {cheese: 'one cup', coriander: 'one teaspoon'};
+  for (var ingredient in recipe) {
+    console.log("this recipe calls for " + recipe[ingredient] + " of " + ingredient );
+  }
+}
